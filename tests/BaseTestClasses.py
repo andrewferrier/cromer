cc4f1509447481736b0275ca649d11ea682b140e
Andrew Ferrier <andrewferrier@example.com>
2015-11-11 00:06:47 +0000

    Make unit tests more repeatable with a fake $HOME - closes #16

--- tests/BaseTestClasses.py
+++ tests/BaseTestClasses.py
@@ -1,15 +1,21 @@
-import unittest
 import os
-from subprocess import Popen, PIPE
-
+import shutil
 import subprocess
+import tempfile
+import unittest
 
+from subprocess import Popen, PIPE
 
 class CromerTestCase(unittest.TestCase):
     COMMAND = os.path.normpath(os.path.join(os.getcwd(), 'cromer'))
 
     def setUp(self):
-        pass
+        self.fake_user_dir = tempfile.mkdtemp(dir='/tmp')
 
     def runAsSubProcess(self, args):
-        return subprocess.run(CromerTestCase.COMMAND + ' ' + args, shell=True, stdout=PIPE, stderr=PIPE)
+        my_env = os.environ.copy()
+        my_env['HOME'] = self.fake_user_dir
+        return subprocess.run(CromerTestCase.COMMAND + ' ' + args, shell=True, stdout=PIPE, stderr=PIPE, env=my_env)
+
+    def tearDown(self):
+        shutil.rmtree(self.fake_user_dir)
