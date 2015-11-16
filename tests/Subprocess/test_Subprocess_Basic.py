15609cc5cc37b047b7d7cf7f6abc9e1a1be1d277
Andrew Ferrier <andrewferrier@example.com>
2015-11-16 16:07:46 +0400

    Set -X to default to 0 - closes #11

--- tests/Subprocess/test_Subprocess_Basic.py
+++ tests/Subprocess/test_Subprocess_Basic.py
@@ -32,14 +32,87 @@ class TestBasic(CromerTestCase):
         self.assertEqual(completed.returncode, 101)
 
     def test_zerotimeout(self):
-        completed = self.runAsSubProcess('-X 0s sleep 2')
+        completed = self.runAsSubProcess('-X 0s sleep 1')
         self.assertEqual(completed.returncode, 0)
 
     def test_zero_defaulttimeout(self):
-        completed = self.runAsSubProcess('sleep 2')
+        completed = self.runAsSubProcess('sleep 1')
         self.assertEqual(completed.returncode, 0)
+
+    def test_maxinterval_true(self):
+        completed = self.runAsSubProcess('true')
+        self.assertEqual(completed.stdout, b'')
+        self.assertEqual(completed.stderr, b'')
+        self.assertEqual(completed.returncode, 0)
+
+    def test_maxinterval_false(self):
+        completed = self.runAsSubProcess('false')
+        self.assertEqual(completed.stdout, b'')
+        self.assertRegex(completed.stderr, b'(?i)hashfile missing')
+        self.assertEqual(completed.returncode, 102)
 
     def test_basicmocksubprocess(self):
         with self.createMockSubprocessFile() as filename:
             self.createMockSubprocessContent(filename)
             completed = self.runAsSubProcess(filename)
+
+    def test_maxinterval_stdout(self):
+        with self.createMockSubprocessFile() as filename:
+            self.createMockSubprocessContent(filename)
+            completed = self.runAsSubProcess(filename)
+            self.assertEqual(completed.returncode, 0)
+            self.assertEqual(completed.stdout, b'')
+            self.assertEqual(completed.stderr, b'')
+            self.createMockSubprocessContent(filename, stdout=True)
+            time.sleep(1)
+            completed = self.runAsSubProcess(filename)
+            self.assertEqual(completed.stdout, b'')
+            self.assertRegex(completed.stderr, b'(?i)max interval')
+            self.assertEqual(completed.returncode, 101)
+
+    def test_maxinterval_stderr(self):
+        with self.createMockSubprocessFile() as filename:
+            self.createMockSubprocessContent(filename)
+            completed = self.runAsSubProcess(filename)
+            self.assertEqual(completed.returncode, 0)
+            self.assertEqual(completed.stdout, b'')
+            self.assertEqual(completed.stderr, b'')
+            self.createMockSubprocessContent(filename, stderr=True)
+            time.sleep(1)
+            completed = self.runAsSubProcess(filename)
+            self.assertEqual(completed.stdout, b'')
+            self.assertRegex(completed.stderr, b'(?i)max interval')
+            self.assertEqual(completed.returncode, 101)
+
+    def test_maxinterval_returncode(self):
+        with self.createMockSubprocessFile() as filename:
+            self.createMockSubprocessContent(filename)
+            completed = self.runAsSubProcess(filename)
+            self.assertEqual(completed.returncode, 0)
+            self.assertEqual(completed.stdout, b'')
+            self.assertEqual(completed.stderr, b'')
+            self.createMockSubprocessContent(filename, returncode=1)
+            time.sleep(1)
+            completed = self.runAsSubProcess(filename)
+            self.assertEqual(completed.stdout, b'')
+            self.assertRegex(completed.stderr, b'(?i)max interval')
+            self.assertEqual(completed.returncode, 101)
+
+    def test_maxinterval_explicit_returncode(self):
+        with self.createMockSubprocessFile() as filename:
+            self.createMockSubprocessContent(filename)
+            completed = self.runAsSubProcess('-X 4s ' + filename)
+            self.assertEqual(completed.returncode, 0)
+            self.assertEqual(completed.stdout, b'')
+            self.assertEqual(completed.stderr, b'')
+            self.createMockSubprocessContent(filename, returncode=1)
+            time.sleep(2)
+            completed = self.runAsSubProcess('-X 4s ' + filename)
+            self.assertEqual(completed.returncode, 0)
+            self.assertEqual(completed.stdout, b'')
+            self.assertEqual(completed.stderr, b'')
+            time.sleep(3)
+            completed = self.runAsSubProcess('-X 4s ' + filename)
+            self.assertEqual(completed.stdout, b'')
+            self.assertRegex(completed.stderr, b'(?i)max interval')
+            self.assertEqual(completed.returncode, 101)
