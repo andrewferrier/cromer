ce09b8e6061472495da9cb4775fd4f4ad8364682
Andrew Ferrier <andrewferrier@example.com>
2016-01-22 10:45:57 +0000

    Remove redundant wait to fix issue of inconsistent timeout - closes #31
    (and add tests to verify.)

--- tests/Subprocess/test_Subprocess_Basic.py
+++ tests/Subprocess/test_Subprocess_Basic.py
@@ -1,11 +1,20 @@
+import tempfile
 import time
 
+from string import Template
 from tests.BaseTestClasses import CromerTestCase
 
 
 class TestBasic(CromerTestCase):
     def setUp(self):
         super(TestBasic, self).setUp()
+
+    def get_time_in_seconds(self):
+        return time.perf_counter()
+
+    def get_file_contents(self, filename):
+        with open(filename, "r") as myfile:
+            return myfile.read()
 
     def test_simple_true(self):
         completed = self.runAsSubProcess('true')
@@ -38,10 +47,34 @@ class TestBasic(CromerTestCase):
         self.assertEqual(completed.returncode, 103)
 
     def test_timeout(self):
+        a = self.get_time_in_seconds()
         completed = self.runAsSubProcess('sleep 2')
+        b = self.get_time_in_seconds()
         self.assertEqual(completed.returncode, 0)
+        self.assertAlmostEqual(a + 2, b, delta=0.5)
         completed = self.runAsSubProcess('-t 1s sleep 2')
+        c = self.get_time_in_seconds()
+        self.assertAlmostEqual(b + 1, c, delta=0.5)
         self.assertEqual(completed.returncode, 101)
+
+    def test_timeout_with_loop_command(self):
+        LOOP_COMMAND = 'bash -c \'for i in `seq 1 3`; do sleep 1; echo -n $$i >> $outputfile; done\''
+
+        with tempfile.NamedTemporaryFile() as named_file:
+            loopCommandFinal = Template(LOOP_COMMAND).substitute({'outputfile': named_file.name})
+            completed = self.runAsSubProcess(loopCommandFinal)
+            self.assertEqual(completed.returncode, 0)
+            self.assertEqual(completed.stdout, b'')
+            self.assertEqual(completed.stderr, b'')
+            self.assertEqual(self.get_file_contents(named_file.name), "123")
+            a = self.get_time_in_seconds()
+            completed = self.runAsSubProcess('-X 1m -t 2s ' + loopCommandFinal)
+            b = self.get_time_in_seconds()
+            self.assertEqual(completed.returncode, 0)
+            self.assertEqual(completed.stdout, b'')
+            self.assertEqual(completed.stderr, b'')
+            self.assertEqual(self.get_file_contents(named_file.name), "1231")
+            self.assertAlmostEqual(a + 2, b, delta=0.5)
 
     def test_timeout_junk(self):
         completed = self.runAsSubProcess('-t blah sleep 2')
@@ -49,11 +82,17 @@ class TestBasic(CromerTestCase):
         self.assertEqual(completed.returncode, 2)
 
     def test_zerotimeout(self):
+        a = self.get_time_in_seconds()
         completed = self.runAsSubProcess('-t 0s sleep 1')
+        b = self.get_time_in_seconds()
+        self.assertAlmostEqual(a + 1, b, delta=0.5)
         self.assertEqual(completed.returncode, 0)
 
     def test_zero_defaulttimeout(self):
+        a = self.get_time_in_seconds()
         completed = self.runAsSubProcess('sleep 1')
+        b = self.get_time_in_seconds()
+        self.assertAlmostEqual(a + 1, b, delta=0.5)
         self.assertEqual(completed.returncode, 0)
 
     def test_basicmocksubprocess(self):
